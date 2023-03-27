0827c7d162f08ca54874d9c92fbb47e0f985a57f
Borislav Atanasov <borislav.atanasov@dci.education>
2023-03-27 17:10:22 +0200

    Session Finished

---
+++ Session 3/assignments/leapYear.js
@@ -0,0 +1,13 @@
+// Write a program that generates a random leap year using a while loop in JavaScript. The program should generate every single digit of the year one by one and check if the generated year is a leap year. If the generated year is not a leap year, the program should start generating again until a leap year is generated.
+
+// A year is considered a leap year if it is divisible by 4, except for years that are divisible by 100. However, years that are divisible by 400 are also leap years. For example, the year 2000 was a leap year, but 1900 was not.
+
+// Your program should do the following:
+
+// Generate a random number between 0 and 9 (inclusive) for the first digit of the year.
+// Generate a random number between 0 and 9 (inclusive) for the second digit of the year.
+// Generate a random number between 0 and 9 (inclusive) for the third digit of the year.
+// Generate a random number between 0 and 9 (inclusive) for the fourth digit of the year.
+// Combine the generated digits to form a year.
+// Check if the year is a leap year using the rules mentioned above. If it is a leap year, output the year and stop the program. If it is not a leap year, start generating again from step 1.
+
---
+++ Session 3/leapYear.js
@@ -0,0 +1,45 @@
+// Write a program that generates a random leap year using a while loop in JavaScript. The program should generate every single digit of the year one by one and check if the generated year is a leap year. If the generated year is not a leap year, the program should start generating again until a leap year is generated.
+
+// A year is considered a leap year if it is divisible by 4, except for years that are divisible by 100. However, years that are divisible by 400 are also leap years. For example, the year 2000 was a leap year, but 1900 was not.
+
+// Your program should do the following:
+
+// Generate a random number between 0 and 9 (inclusive) for the first digit of the year.
+// Generate a random number between 0 and 9 (inclusive) for the second digit of the year.
+// Generate a random number between 0 and 9 (inclusive) for the third digit of the year.
+// Generate a random number between 0 and 9 (inclusive) for the fourth digit of the year.
+// Combine the generated digits to form a year.
+// Check if the year is a leap year using the rules mentioned above. If it is a leap year, output the year and stop the program. If it is not a leap year, start generating again from step 1.
+
+
+let year;
+let isLeapYear = false;
+console.log(`Example`);
+console.log(`Example`);
+console.log(`Example`);
+while (!isLeapYear) {
+    year = "";
+    for (let i = 0; i < 4; i++) {
+        let singleDigit = Math.floor(Math.random() * 10);
+        year += singleDigit;
+    }
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        console.log(`Year ${year} is a leap year`);
+        isLeapYear = true;
+
+    }
+    else {
+        console.log(`Year ${year} is not a leap year`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
